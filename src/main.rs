03b181cc550d8cedf55fdbd010ce40f4cdbb4bed
cerdelen <cedric.erdelen@gmail.com>
2025-09-24 20:04:33 +0200

    Quick little Panagram Counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,97 @@
+use std::{env, fs};
+
+
+fn get_file(path: &str) -> std::io::Result<Vec<String>> {
+    let content = fs::read_to_string(path)?;
+
+    let trimmed_content = content.trim();
+    let inner = trimmed_content.strip_suffix("]").and_then(|s| s.strip_prefix("[")).unwrap_or(trimmed_content);
+
+    let mut vec: Vec<String>= inner
+        .split('\n')
+        .map(|s| s.trim())
+        .map(|s| s.trim_matches(','))
+        .map(|s| s.trim_matches('"'))
+        .map(|s| s.to_string())
+        .collect();
+
+    if vec.last().is_some_and(|s| s.is_empty()) {
+        vec.pop();
+    }
+    if vec.first().is_some_and(|s| s.is_empty()) {
+        vec.remove(0);
+    }
+
+    Ok(vec)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Wrong amt of input arguments! Only provide the Path to the File!");
+        std::process::exit(1);
+    }
+
+    let objects = match get_file(&args[1]) {
+        Ok(vec) => vec,
+        Err(e) => {
+            eprintln!("Error Parsing the File: {e}");
+            std::process::exit(1);
+        },
+    };
+
+    let mut panagram_counter = PanagramCounter::default();
+
+    for s in objects {
+        match is_kind_of_panagram(s) {
+            KindOfPanagram::None => panagram_counter.none += 1,
+            KindOfPanagram::ImperfectPanagram => panagram_counter.imperfect += 1,
+            KindOfPanagram::Perfect => panagram_counter.perfect += 1,
+        }
+    }
+
+    println!("Result of Panagram Counter by Cerdelen");
+    println!("");
+    println!("Amount of non Panagrams:        {:>3}", panagram_counter.none);
+    println!("Amount of imperfect Panagrams:  {:>3}", panagram_counter.imperfect);
+    println!("Amount of perfect Panagrams:    {:>3}", panagram_counter.perfect);
+}
+
+enum KindOfPanagram {
+    None,
+    Perfect,
+    ImperfectPanagram
+}
+
+#[derive(Debug, Default)]
+struct PanagramCounter {
+    none: u16,
+    perfect: u16,
+    imperfect: u16,
+}
+
+// Time Complexity is O(n) with n == size of String.
+// Space Complexity is O(1). Constant Space complexity.
+fn is_kind_of_panagram(s: String) -> KindOfPanagram {
+    let mut char_map: [u8; 26] = [0; 26];
+    let mut out = KindOfPanagram::Perfect;
+
+    for char in s.chars() {
+        let lower_case_ascii_char = char.to_ascii_lowercase();
+
+        if lower_case_ascii_char.is_alphabetic() {
+            char_map[(lower_case_ascii_char as u8 - b'a') as usize] += 1;
+        }
+    }
+
+    for letter in char_map {
+        match letter {
+            0 => return KindOfPanagram::None,
+            1 => continue,
+            _ => out = KindOfPanagram::ImperfectPanagram,
+        }
+    };
+    out
 }
